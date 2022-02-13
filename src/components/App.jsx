3f819c307f0419058734c60c184239b36311b2b2
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-02-13 01:37:56 +0100

    Start with backbone componenets.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer.jsx";
+import Frame from "./Frame.jsx";
+
+const App = ()=> {
+    return(
+        <div>
+            <Frame />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
